7218f2660e9daca7a4b796b196b5fd862fe353f6
Emily Vogel <eav990@gmail.com>
2020-10-27 16:23:18 -0400

    Try for root async

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,12 @@
-import { Controller, Get, HttpCode, Req } from '@nestjs/common';
-import { UseGuards } from '@nestjs/common/decorators/core/use-guards.decorator';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('config')
-  @HttpCode(200)
-  getConfig() {
-    return { clientID: process.env.GOOGLE_CLIENT_ID };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,24 +7,39 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     ProjectsModule,
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     // ElasticsearchModule.register({
     //   node: process.env.ELASTIC_CONNECTION_URI,
     // }),
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: process.env.MONGODB_CONNECTION_URI,
-      database: process.env.DATABASE,
-      entities: [
-        __dirname + '/**/*.entity{.ts,.js}',
-      ],
-      useUnifiedTopology: true,
-      useNewUrlParser: true
+    // TypeOrmModule.forRoot({
+    //   type: 'mongodb',
+    //   url: process.env.MONGODB_CONNECTION_URI,
+    //   database: process.env.DATABASE,
+    //   entities: [
+    //     __dirname + '/**/*.entity{.ts,.js}',
+    //   ],
+    //   useUnifiedTopology: true,
+    //   useNewUrlParser: true
+    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mongodb',
+        url: configService.get('MONGODB_CONNECTION_URI'),
+        database: configService.get('DATABASE'),
+        entities: [
+          __dirname + '/**/*.entity{.ts,.js}',
+        ],
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+      }),
     }),
   ],
   controllers: [AppController],
